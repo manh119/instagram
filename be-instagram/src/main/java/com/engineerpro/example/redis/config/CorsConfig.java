ea82c1d37e9fa5fcd385e4f76c69d6199dace31f
Manh <manhnatria2@gmail.com>
2025-09-13 16:12:47 +0700

    fix cors for fe prod

--- be-instagram/src/main/java/com/engineerpro/example/redis/config/CorsConfig.java
+++ be-instagram/src/main/java/com/engineerpro/example/redis/config/CorsConfig.java
@@ -21,8 +21,8 @@ public class CorsConfig {
                 "http://localhost:3001", // Frontend production port
                 "http://localhost:5173", // Vite default
                 "http://localhost:8080", // Your backenkend port
-                "http://14.225.204.207:80" // Your backend production IP
-        ));
+                "https://14.225.204.207:80", // Your backend production IP
+                "https://insta-demo-manh.vercel.app"));
 
         // Allow common HTTP methods
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
